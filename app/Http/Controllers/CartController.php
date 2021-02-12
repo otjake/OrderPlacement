91e8cc7333b5589aed688920fae1d0e6af07a3f0
otjake <jaketuriacada@gmail.com>
2021-02-12 09:38:17 +0100

    refactor

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\Cart;
+use Illuminate\Support\Facades\Validator;
+
+class CartController extends Controller
+{
+    function AddToCart(Request $request){
+
+        $cart=new Cart;
+
+        $rules=array(
+            'customer_email'=>'required|email:dns,rfc',
+        );
+
+        $validator=Validator::make($request->all(),$rules);
+
+        if ($validator->fails()) {
+            return response()->json(['error'=>$validator->errors()->all()]);
+        }
+
+        //cart orders[collect product_id]
+        $cart->product_id=$request->product_id;
+        $items=explode(",",$request->product_id);
+        $items_count=count($items);
+        $cart->total_order_quantity=$items_count;
+
+
+        //cart orders quantity[collect quantity for each product]
+        $cart->product_quantity=$request->product_quantity;
+        $items_quantity=explode(",",$request->product_quantity);
+        $items_quantity_count=count( $items_quantity);
+
+
+        //making sure product entered and their no of quantity entered match
+        if($items_count !== $items_quantity_count ){
+            return response()->json(['error'=>"product inputs do not match quantity inputs,make sure inputs are comma separated"],401);
+
+        }
+
+        //collecting each products price and making sure they are integers
+        $price_array=array();
+        foreach ($items as $item){
+            if(intval($item)) {
+                $test = Product::where('product_id', $item)->get();
+                $prices = $test[0]->price;
+                array_push($price_array, $prices);
+            }
+            else{
+                return response()->json(['error'=>"Non-numeric input product id,make sure inputs are comma separated"],401);
+
+            }
+        }
+
+
+        $quantity_array=array();
+
+        //validating
+        foreach ($items_quantity as $item_q){
+            if(intval($item_q)) {
+                array_push($quantity_array, $item_q);
+            }
+            else{
+                return response()->json(['error'=>"Non-numeric input in product quantity,make sure inputs are comma separated"],401);
+
+            }
+        }
+
+
+        //collecting and multiplying each product quantity with their_price
+        $total = array();
+        foreach($price_array as $key => $price){
+            $total[$key] = $price * $quantity_array[$key];
+        }
+
+        $cart->customer_email=$request->customer_email;
+        $cart->total_order_price=array_sum($total);
+
+        $result=$cart->save();
+       if($result){
+           return response()->json(array("id"=> $cart->id,"Order_Total_price"=>$cart->total_order_price,'success'=>'Order made Successfully.'),200);
+       }else{
+           return ["An error occurred"];
+       }
+
+    }
+}
---
+++ database/migrations/2021_02_11_123913_create_carts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->increments('cart_id');
+            $table->string('customer_email');
+            $table->text('product_id');
+            $table->text('product_quantity');
+            $table->integer('total_order_price');
+            $table->integer('total_order_quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+}
